a3e0d8c67b3b16b409f2d301b5c86dff2b9bb1c4
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2024-10-01 19:59:59 -0300

    Padronizando altura dos cards e deixando button fixo

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -1,28 +1,33 @@
 import { Card, CardHeader, CardBody, CardFooter, Heading } from '@chakra-ui/react'
 import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 
 export default ({title, text, textbtn, courseId}) => {
     const urlDestino = `/cursos/curso?idCurso=${courseId}`
     return (
         <>
-            <Card className='card' width={350} title={title} display='flex' justifyContent='center' alignItems='center' flexDir='column' courseId={courseId}>
+            <Card className='card' width={350} minHeight={220} title={title} display='flex' justifyContent='center' alignItems='center' flexDir='column' courseId={courseId} position='relative'>
                 <CardHeader p='4'>
                     <Heading className='card-title' size='md'>{title}</Heading>
                 </CardHeader>
 
-                <CardBody display='flex' justifyContent='center' alignItems='center' flexDir='column'>
+                <CardBody display='flex' justifyContent='start' alignItems='center' flexDir='column'>
                     <p className='card-description' style={{
                         display: '-webkit-box',
-                        WebkitLineClamp: 3,
+                        WebkitLineClamp: 2,
                         WebkitBoxOrient: 'vertical',
                         overflow: 'hidden',
                         textOverflow: 'ellipsis'
                     }}>{text}</p>
-                    <Link to={urlDestino}>
-                        <Button size='md' marginTop='5' colorScheme='teal'>{textbtn} ▶️</Button>
-                    </Link>
+                    
                 </CardBody>
+
+                <Box position='absolute' bottom='1.75rem'>
+                    <Link to={urlDestino}>
+                        <Button size='md' marginTop='5' colorScheme='teal'>{textbtn}</Button>
+                    </Link>
+                </Box>
 
             </Card>
         </>
